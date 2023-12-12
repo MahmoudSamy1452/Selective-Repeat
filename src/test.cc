0e41595319b28b8ad79476294b6d40152aa06f07
AbdullahAyman02 <14712020102049@stud.cu.edu.eg>
2023-12-12 20:46:54 +0200

    Modifications

--- src/test.cc
+++ src/test.cc
@@ -21,9 +21,17 @@
 
 
 Define_Module(Test);
-std::vector<std::pair<std::string, std::string>> dataVector;
-int sf,sn,sl;
-int rf,rn,rl;
+std::vector<std::pair<std::string, std::string>> in_buff;       //size is retrieved as parameter from omnetpp.ini
+std::vector<std::pair<std::string, std::string>> out_buff;      //size is retrieved as parameter from omnetpp.ini
+std::queue<std::pair<std::string,std::string>> networkLayer;
+int WS, WR, max_seq_s, max_seq_r;                               //retrieved as parameter from omnetpp.ini (ws)
+int ack_expected, next_frame_to_send;
+int frame_expected, too_far;
+bool no_nak;                                                    //at receiver side
+double PT, DD, TD, ED;
+#define DATA 0
+#define ACK 1
+#define NACK 2
 /*vector<MyMessage_Base *> receiverFinalResult;
 vector<MyMessage_Base *> receiverBuffer ;
 vector<bool> arrived(3);
@@ -31,11 +39,111 @@ vector<int> sequenceArrived(3);
 vector<bool> arrivedSender(3);
 vector<int> sequenceArrivedSender(3);*/
 
+void readFile(std::string file_name)
+{
+    std::ifstream inputFile(file_name);
+    std::vector<std::string> textInput;
+
+    if (!inputFile.is_open())
+    {
+        EV << "Error opening file!" << endl;
+        return;
+    }
+
+    std::string line;
+    while (std::getline(inputFile, line))
+    {
+        char number[4];
+        char restOfString[256];
+
+        if (std::sscanf(line.c_str(), "%s %255[^\n]", &number, restOfString) == 2)
+        {
+            networkLayer.push(std::make_pair(number, std::string(restOfString)));
+        }
+    }
+
+    inputFile.close();
+}
+
 void modifyMessage(std::string &message,int index,int modificationBit)
 {
-
     message[index] ^= 1 << modificationBit;
+}
 
+bool between(int a, int b, int c)
+{
+    return (((a <= b) && (b < c)) || ((c < a) && (a <= b)) || ((b < c) && (c < a)));
+}
+
+void circularSum (int& value, int size)
+{
+    value = (value + 1) % size;
+}
+
+void sendFrame(int frameKind, int frame_nr, bool resend = false)//0 is data, 1 is ack, 2 is nack
+{
+    if(frameKind == DATA)// data, only sender will enter here 
+    {
+        //data
+        std::string initalString = out_buff[frame_nr].second;
+        std::string errors = out_buff[frame_nr].first;
+
+        char checkSum = calcCheckSum(initalString);
+        EV << "checksum is: "<<checkSum<<endl;
+
+        if(errors[3] == '1' && !resend)
+        {
+            EV<<" introducing modification "<<endl;
+            int modificationBit = int(uniform(0, 7));
+            int index = int(uniform(0, initalString.size()));
+            modifyMessage(initalString,index,modificationBit);
+        }
+
+        std::string message = byteStuffing(initalString);
+        EV<<"sent message is: " << message<<endl;
+
+        MyMessage_Base * msg = new MyMessage_Base("message");
+        msg->setM_Payload(message.c_str());
+        msg->setMycheckbits(checkSum);
+        storedMsg = msg;
+
+        double PT = getParentModule()->par("PT");
+        double DD = getParentModule()->par("DD");
+        double TD = getParentModule()->par("TD");
+        double ED = getParentModule()->par("ED");
+
+        if(errors[0] == '1' && !resend)
+        {
+            //delay
+
+            EV <<"PT is: "<<PT<<endl;
+            scheduleAt(simTime()+PT, msg);
+        } else
+
+        if(errors[1] == '1' && !resend)
+        {
+            //duplication
+        }
+        if(errors[2] == '1' && !resend)
+        {
+            //loss
+        }
+        send(msg, "out");
+    }
+    else if(frameKind == ACK) 
+    {
+        //ack
+        MyMessage_Base * msg = new MyMessage_Base("ack");
+        msg->setM_Payload("ack");
+        send(msg, "out");
+    }
+    else if(frameKind == NACK)
+    {
+        //nack
+        MyMessage_Base * msg = new MyMessage_Base("nack");
+        msg->setM_Payload("nack");
+        send(msg, "out");
+    }
 }
 
 /*void introduceErrors(std::string errors,std::string message)
@@ -117,31 +225,20 @@ char calcCheckSum(std::string message)
     return result;
 }
 
-
-
-
 void Test::initialize()
 {
-    std::ifstream inputFile("input.txt");
-    std::vector<std::string> textInput;
-
-    if (!inputFile.is_open())
-    {
-            EV << "Error opening file!" << endl;
-            return;
-    }
-
-    std::string line;
-    while (std::getline(inputFile, line))
-    {
-        char number[4];
-        char restOfString[256];
-
-        if (std::sscanf(line.c_str(), "%s %255[^\n]", &number, restOfString) == 2)
-          {
-             dataVector.push_back(std::make_pair(number, std::string(restOfString)));
-          }
-    }
+    // Resize buffers according to parameter WS
+    WS = getParentModule()->par("WS");     
+    WR = getParentModule()->par("WR");
+    max_seq_s = (WS * 2);       //max sequence number for sender
+    max_seq_r = (WR * 2);       //max sequence number for receiver
+    inBuffer.resize(WS);
+    outBuffer.resize(WR);
+    ack_expected = 0;
+    next_frame_to_send = 0;
+    frame_expected = 0;
+    too_far = (WR + 1)/2;
+    no_nak = true;
 
     // sending the first message
     if(strcmp("sender",getName()) == 0)
